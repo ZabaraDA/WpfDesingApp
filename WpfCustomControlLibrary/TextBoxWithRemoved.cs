a5203b896f892f74eeef5904859be17f627a1210
ZabaraDA <72722304+ZabaraDA@users.noreply.github.com>
2022-11-20 20:17:05 +0500

    Add files via upload

---
+++ WpfCustomControlLibrary/TextBoxWithRemoved.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WpfCustomControlLibrary
+{
+    /// <summary>
+    /// Выполните шаги 1a или 1b, а затем 2, чтобы использовать этот пользовательский элемент управления в файле XAML.
+    ///
+    /// Шаг 1a. Использование пользовательского элемента управления в файле XAML, существующем в текущем проекте.
+    /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он 
+    /// будет использоваться:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WpfCustomControlLibrary"
+    ///
+    ///
+    /// Шаг 1б. Использование пользовательского элемента управления в файле XAML, существующем в другом проекте.
+    /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он 
+    /// будет использоваться:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WpfCustomControlLibrary;assembly=WpfCustomControlLibrary"
+    ///
+    /// Потребуется также добавить ссылку из проекта, в котором находится файл XAML,
+    /// на данный проект и заново выполнить построение во избежание ошибок компиляции:
+    ///
+    ///     Щелкните правой кнопкой мыши нужный проект в обозревателе решений и выберите
+    ///     "Добавить ссылку"->"Проекты"->[Выберите этот проект]
+    ///
+    ///
+    /// Шаг 2)
+    /// Продолжайте дальше и используйте элемент управления в файле XAML.
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    [TemplatePart(Name = "PART_Button", Type = typeof(Button))]
+    public class TextBoxWithRemoved : TextBox
+    {
+        
+        static TextBoxWithRemoved()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TextBoxWithRemoved), new FrameworkPropertyMetadata(typeof(TextBoxWithRemoved)));
+        }
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            var button = this.GetTemplateChild("PART_Button") as Button;
+
+            if (button != null)
+            {
+                button.Click += button_Click;
+            }      
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Text = string.Empty;
+        } 
+    }
+}
